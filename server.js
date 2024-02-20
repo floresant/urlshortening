ac21842ba01b83640c0ffa7e56c7837a1b53938b
floresant <136223490+floresant@users.noreply.github.com>
2024-02-20 10:07:53 -0700

    Installed express, edited server.js to listen to port 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from express;
+
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app is listening on port ${port}.`)
+})
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the page!");
+})
